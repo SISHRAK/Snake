da9a96bcb3d148b778b20777156c35013cb7fcd8
Arseniy Barashko <94114454+SISHRAK@users.noreply.github.com>
2024-04-21 02:15:17 +0300

    Add files via upload

---
+++ src/Snake.java
@@ -0,0 +1,24 @@
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+public class Snake extends JFrame {
+    public Snake(){
+        initUI();
+    }
+    private void initUI(){
+        add(new Board());
+        setResizable(false);
+        pack();
+
+        setTitle("Зомби Апокалипсис");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+    public static void main(String[] args){
+        EventQueue.invokeLater(()->{
+            JFrame ex = new Snake();
+            ex.setVisible(true);
+        });
+    }
+}
